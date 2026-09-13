084067c6c59d0b476262bc96bcc42dfbcd6d67f2
nonodza <skaynono@gmail.com>
2026-09-13 22:36:21 +0100

    Modify MainActivity to set blue background and toast
    Changed background color to blue and updated toast message.

--- app/src/main/java/com/bls/rendezvous/MainActivity.java
+++ app/src/main/java/com/bls/rendezvous/MainActivity.java
@@ -3,12 +3,20 @@ package com.bls.rendezvous;
 import android.os.Bundle;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toast.makeText(this, "BLS V2 INSTALLED!", Toast.LENGTH_LONG).show();
+        
+        // خلفية زرقاء
+        View view = new View(this);
+        view.setBackgroundColor(Color.parseColor("#2196F3"));
+        setContentView(view);
+        
+        // رسالة
+        Toast.makeText(this, "BLS V2 - 21:30 NEW!", Toast.LENGTH_LONG).show();
     }
 }
