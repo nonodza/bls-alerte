d9c108b7c87d5cd21121bbee2e3af22e79ce83b9
nonodza <skaynono@gmail.com>
2026-09-17 18:43:41 +0100

    Rename MainActivity to MainActivityOld

--- app/src/main/java/com/bls/rendezvous/MainActivityOld.java
+++ app/src/main/java/com/bls/rendezvous/MainActivityOld.java
@@ -10,8 +10,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.os.Handler;
-
-public class MainActivity extends Activity {
+public class MainActivityOld extends Activity {
     private final int NAVY = Color.rgb(25, 35, 70);
     private final int GRAY = Color.rgb(105, 120, 145);
 
