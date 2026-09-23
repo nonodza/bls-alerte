6c432b87aa25adad90db4ee6a0b5933fd957c4f4
nonodza <skaynono@gmail.com>
2026-09-23 21:37:06 +0100

    Add Toast import to MainActivity

--- app/src/main/java/com/bls/rendezvous/MainActivity.java
+++ app/src/main/java/com/bls/rendezvous/MainActivity.java
@@ -18,6 +18,7 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
